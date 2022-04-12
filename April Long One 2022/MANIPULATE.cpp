4ece91faa0800903157c376dc8f237657685394e
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-04-12 06:54:13 +0530

    Update MANIPULATE.cpp

--- April Long One 2022/MANIPULATE.cpp
+++ April Long One 2022/MANIPULATE.cpp
@@ -1,2 +1,23 @@
-.
-.
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>> T;
+	while(T-->0)
+	{
+	    int X;
+	    int Y;
+	    cin>> X >>Y;
+	    if(Y>X)
+	    {
+	        cout<< "NO" << endl;
+	    }
+	    else
+	    {
+	        cout<< "YES" << endl;
+	    }
+	}
+	return 0;
+}
