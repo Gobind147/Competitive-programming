632a78cdf2dc29316cb8ad73cda42efc636383e7
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-04-20 22:52:46 +0530

    Create WATERREQ.cpp

---
+++ CodeChef Starters 35/WATERREQ.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>> T;
+	while(T-->0)
+	{
+	    int X;
+	    cin>> X;
+	    int Y = 2*X;
+	    cout << Y << endl;
+	}
+	return 0;
+}
