c68cab51157738047b8e41271e819b4316dd71fb
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-04-27 22:03:11 +0530

    Create TALLER.cpp

---
+++ CodeChef Starters 36/TALLER.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>> T;
+	while(T-->0)
+	{
+	    int A;
+	    int B;
+	    cin>> A >>B;
+	    if(A>B)
+	    {
+	        cout << "A"<< endl;
+	    }
+	    else
+	    {
+	        cout << "B" << endl;
+	    }
+	}
+	return 0;
+}
