3a231498f26dc2995c096e12782b139d1fb77d4e
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-04-27 22:05:17 +0530

    Create PRESENTS.cpp

---
+++ CodeChef Starters 36/PRESENTS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>> T;
+	while(T-->0)
+	{
+	    int N;
+	    cin>> N;
+	    int k = N/5;
+	    if(N<=4)
+	    {
+	        cout << N << endl;
+	    }
+	    else 
+	    {
+	        cout << N-k << endl;
+	    }
+	}
+	return 0;
+}
