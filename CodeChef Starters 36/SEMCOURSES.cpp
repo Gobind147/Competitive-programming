a8bdae94e338cabd05289241318e551f5d75b81d
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-04-27 22:03:54 +0530

    Create SEMCOURSES.cpp

---
+++ CodeChef Starters 36/SEMCOURSES.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>> T;
+	while(T-->0)
+	{
+	    int X;
+	    int Y;
+	    int Z;
+	    cin>> X >> Y>> Z;
+	    int total = X*Y*Z;
+	    cout<< total << endl;
+	}
+	return 0;
+}
