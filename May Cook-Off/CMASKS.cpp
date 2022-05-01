69176bfaa3c99d4c5dd89ec94cbf8a091ca41f84
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-01 22:55:44 +0530

    Create CMASKS.cpp

---
+++ May Cook-Off/CMASKS.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>> T;
+	while(T--)
+	{
+	    int X,Y;
+	    cin>> X >> Y;
+	    if(100*X>10*Y)
+	    {
+	        cout << "Cloth" << endl;
+	    }
+	    else if(100*X==10*Y)
+	    {
+	        cout << "Cloth" << endl;
+	    }
+	    else
+	    {
+	        cout << "Disposable" << endl;
+	    }
+	}
+	return 0;
+}
