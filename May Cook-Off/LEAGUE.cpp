93eda7c11c796182247bf289a0570a2dcc332644
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-01 23:06:30 +0530

    Create LEAGUE.cpp

---
+++ May Cook-Off/LEAGUE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin >> T;
+	while(T--)
+	{
+	    int N;
+	    cin >> N;
+	    N = N/2;
+	    cout << 3*N << endl;
+	}
+	return 0;
+}
