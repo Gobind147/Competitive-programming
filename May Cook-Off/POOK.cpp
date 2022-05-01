eb6e905bf40265076ac3dfd251bf9ba0d15d278e
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-01 23:07:20 +0530

    Create POOK.cpp

---
+++ May Cook-Off/POOK.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    if(n==1)
+	    {
+	        cout << "1" << endl;
+	    }
+	    else if(n==2)
+	    {
+	        cout << "1" << endl;
+	    }
+	    else if(n==3)
+	    {
+	        cout << "2" << endl;
+	    }
+	    else
+	    {
+	        cout << n << endl;
+	    }
+	}
+	return 0;
+}
