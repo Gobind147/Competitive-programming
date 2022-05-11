180a5b1610b9a413b27c6ea1fa0f4869785f419f
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-11 23:05:32 +0530

    Create SUBSCRIBE.cpp

---
+++ CodeChef Starters 38/SUBSCRIBE.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>> t;
+	while(t--)
+	{
+	    long long x;
+	    cin >> x;
+	    if(x>30)
+	    {
+	        cout<< "YES" << endl;
+	    }
+	    else
+	    {
+	        cout << "NO" << endl;
+	    }
+	}
+	return 0;
+}
