50292c5578bdc5c252012b65a45492d00824d914
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-15 23:16:03 +0530

    Create TODOLIST.cpp

---
+++ codechef May Lunchtime 2022/TODOLIST.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>> t;
+	
+	while(t--)
+	{
+	    int n;
+	    cin >> n ;
+	    int arr[n];
+	    for(int i = 0; i<n;i++)
+	    {
+	        cin >> arr[i];
+	    }
+	    int count = 0;
+	    for(int j = 0; j<n;j++)
+	    {
+	        if(arr[j]>=1000)
+	        {
+	            count++;
+	        }
+	    }
+	    cout << count << endl;
+	}
+	return 0;
+}
+	    
+	    
