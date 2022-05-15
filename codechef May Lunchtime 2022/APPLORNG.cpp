a9d8034b3c327fef9ff6d321fbc7114aa146b2be
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-15 22:53:05 +0530

    Create APPLORNG.cpp

---
+++ codechef May Lunchtime 2022/APPLORNG.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	int a;
+	int b;
+	cin >> x >> a >> b;
+	if(x >= a + b)
+	{
+	    cout << "yes" << endl;
+	}
+	else
+	{
+	    cout << "no" << endl;
+	}
+	return 0;
+}
