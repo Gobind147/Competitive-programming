ec78b9f55b9cfb6262e245640fde3433c5ad3c2c
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-18 23:25:17 +0530

    Create MY1STCONTEST.cpp

---
+++ CodeChef Starters 39/MY1STCONTEST.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	    int n,a,b;
+	    cin>> n >> a >> b;
+	    int k =n-a;
+	    int l = k-b;
+	    cout << k << " " << l << endl;
+	
+	return 0;
+}
