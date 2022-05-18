b8c9e1cefe79344da414b7f7392b0070e588d7ee
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-18 23:27:09 +0530

    Create RECENTCONT.cpp

---
+++ CodeChef Starters 39/RECENTCONT.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    string s[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin >> s[i];
+	    }
+	    int count1 =0;
+	    int count2 = 0;
+	    for(int i=0;i<n;i++)
+	    {
+	    if(s[i]=="START38")
+	    {
+	        count1 = count1 + 1;
+	    }
+	    else if(s[i]=="LTIME108")
+	    {
+	        count2 = count2 + 1;
+	    }
+	    }
+	    cout << count1 << " " << count2 << endl;
+	}
+	return 0;
+}
