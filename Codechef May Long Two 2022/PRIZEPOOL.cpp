4df9172b5d2adc201ddb7426860def1e8aa19c6d
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-05-23 21:45:25 +0530

    Create PRIZEPOOL.cpp

---
+++ Codechef May Long Two 2022/PRIZEPOOL.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int x,y;
+        cin >> x >> y;
+        int count = 10*x + 90*y;
+        cout << count << endl;
+        
+    }
+	// your code goes here
+	return 0;
+}
