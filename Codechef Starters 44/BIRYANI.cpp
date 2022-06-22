400ee8dd92c19cec87466c41832eb19141753c2a
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-06-22 23:13:46 +0530

    Create BIRYANI.cpp

---
+++ Codechef Starters 44/BIRYANI.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long T;
+    cin >> T;
+    while(T--)
+    {       
+        long long X;
+        long long Y;
+        cin >> X >>Y;
+        cout << X*Y << endl;
+    }
+    return 0;
+}
