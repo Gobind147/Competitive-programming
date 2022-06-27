348a481e75892fc4df9c061b6ad9c6e80231ae3e
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-06-27 15:55:26 +0530

    Create FIT.cpp

---
+++ CODECHEF June long 2/FIT.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long T;
+    cin >> T;
+    while(T--)
+    {       
+        long long X;
+        cin>> X;
+        cout << 10*X << endl;
+    }
+    return 0;
+}
