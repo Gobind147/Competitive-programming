4f9caf429782fa5cb837dea6444149c763100ca0
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-07-02 16:52:57 +0530

    Create BESTOFTWO.cpp

---
+++ Codechef starter 45/BESTOFTWO.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	long long T;
+    cin >> T;
+    while(T--)
+    {       
+        long long X;
+        long long Y;
+        cin >> X >>Y;
+        if(X>=Y)
+        {
+            cout << X << endl;
+        }
+        else
+            cout << Y << endl;
+    }
+    return 0;
+}
