3c38c0cd003841b932272dd400fd51d32c2e2650
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-07-04 20:08:53 +0530

    Create XORPREM.cpp

---
+++ Codechef July Cook-Off 2022/XORPREM.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        long long count =0;
+        if(n<=3){
+            cout<<-1<<endl;
+            continue;
+            count++;
+        }
+        long long k=0;
+        for(int i=n;i>=5;i--)
+        cout<<i<<" ";
+        k++;
+        cout<<1<<" "<<2<<" "<<4<<" "<<3<<endl;
+    }
+}
