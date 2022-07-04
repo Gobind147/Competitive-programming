5fe5dc9940c0805041498dcfcd2a60d004f18cee
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-07-04 20:07:31 +0530

    Create PERMED.cpp

---
+++ Codechef July Cook-Off 2022/PERMED.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+int main(){
+long long t;
+cin>>t;
+while(t--){
+long long n;
+long long count=0;
+int arr[n];
+long long sum =0;
+cin>>n;
+long long pr=n;
+long long qr=1;
+while(pr>qr){
+    cout << pr-- <<" "<< qr++ <<" ";
+}
+if(pr==qr)cout<<qr;
+
+else{
+}
+cout<<endl;
+}
+}
