7348861eedb07b58b0b21b3bbe13aac35a6eb4ad
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2022-07-14 16:10:14 +0530

    Create MINCOINSREQ.cpp

---
+++ Codechef  Starters 47/MINCOINSREQ.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    int count = x%10;
+	    cout<<count<<endl;
+	}
+	return 0;
+}
