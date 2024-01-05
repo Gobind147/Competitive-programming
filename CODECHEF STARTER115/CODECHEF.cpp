6b88060b4ba18d19838d3e1d8d1f94758196ef36
Gobind Kumar <93241765+Gobind147@users.noreply.github.com>
2024-01-05 22:14:15 +0530

    Create CODECHEF.cpp

---
+++ CODECHEF STARTER115/CODECHEF.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+       int N;
+       cin>>N;
+       if(N%4==0)
+       cout<<"YES"<<endl;
+       else
+       cout<<"NO"<<endl;
+}
